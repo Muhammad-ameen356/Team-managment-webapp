f13191bdf604960f633b8c370a9997fe3d4fa6db
muhammad ameen <muhammadameenh1@gmail.com>
2021-08-18 23:55:20 +0500

    Team managment Project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Todo: For Signup and save Data in localStorage
 signup = () => {
     const signupfullname = document.getElementById('signupfullname').value;
     const signupemailid = document.getElementById('signupemailid').value;
@@ -12,28 +13,46 @@ signup = () => {
     // Push Data Usin Array 
     // var person = [signupfullname, signupemailid, signuppassword]
 
+    // Validation for empty value and email
     // if ((signupfullname.length && signupemailid.length && signuppassword.length) > 0 && ((signupemailid.indexOf("@") + 1) !== signupemailid.length) && signupemailid.indexOf("@") > -1) {
-        
-        var signupdata = JSON.parse(localStorage.getItem("persons")) || [];
-        signupdata.push(person);
-        localStorage.setItem("persons", JSON.stringify(signupdata));
-        // return window.location = "./login.html";
-        
+
+    var signupdata = JSON.parse(localStorage.getItem("persons")) || [];
+    signupdata.push(person);
+    localStorage.setItem("persons", JSON.stringify(signupdata));
+    // return window.location = "./login.html";
+
     // }
     //// testing
     // console.log(signupemailid.indexOf("@") + 1);
     // console.log(signupemailid.length);
 }
 
-
+////Todo: Retrive Data From localStorage and Match email and password.
+////Todo: if email and password matched user go on the team page 
 login = () => {
-    let i = 0;var aaa = JSON.parse(localStorage.getItem("persons"))
-    // console.log(aaa[0].email)
 
-    for (i; i<=aaa.length - 1; i++){
-        console.log(aaa[i].name)
+    let loginnameoremail = document.getElementById('loginnameoremail').value;
+    let loginpassword = document.getElementById('loginpassword').value;
+
+    const dataforlogin = JSON.parse(localStorage.getItem("persons"))
+    // console.log(dataforlogin[0].password);
+    let found = false;
+    // Loop for searching data in Array
+    for (let i = 0; i <= dataforlogin.length - 1; i++) {
+        if ((dataforlogin[i].email == loginnameoremail || dataforlogin[i].name == loginnameoremail) && dataforlogin[i].password == loginpassword) {
+            found = true;
+            console.log("Login email and password matched");
+            window.location.href = "./teams.html"
+        }
     }
+    if (found == false) {
+        console.log("Not Found");
+    }
+    // var aaa = JSON.parse(localStorage.getItem("persons"))
+    // console.log(aaa[0].email)  
 }
 
-// var aaa = JSON.parse(localStorage.getItem("persons"))
-// console.log(aaa[0].email)
+
+
+
+
