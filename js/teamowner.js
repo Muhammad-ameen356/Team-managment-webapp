b117755b19006296a49fc4f567abc71bd61a15b4
muhammad ameen <muhammadameenh1@gmail.com>
2021-08-31 02:57:41 +0500

    Team managment Project

--- js/teamowner.js
+++ js/teamowner.js
@@ -1,21 +1,45 @@
-function settingteamGetData(){
-    
-    let seca = JSON.parse(sessionStorage.getItem("email"));
+function settingteamGetData() {
+    let teamdata = JSON.parse(localStorage.getItem("addteam")) || [];
+    let index = sessionStorage.getItem("index")
+    let emailfromindex = teamdata[index].email
     let html = '';
-    seca.forEach((item) => {
+    emailfromindex.forEach((item, ii) => {
         html += `<div class="col-md-4" >
-                    <li>
-                        ${item}
-                    </li>
+                    <li class="mainmemberli">
+                       <span class="memberli"> ${item}  <span><i onclick="removeonemember(${ii})" class="bi bi-x-circle-fill"></i></span></span>
+                    </li> 
                 </div>`
     });
     document.getElementById('memberownerview').innerHTML = html;
 }
 
-function deleteteam(){
+function removeonemember(ii) {
     let teamdata = JSON.parse(localStorage.getItem("addteam")) || [];
     let index = sessionStorage.getItem("index")
-    teamdata.splice(index, 1);   
+    teamdata[index].email.splice(ii, 1)
+    localStorage.setItem("addteam", JSON.stringify(teamdata));
+    settingteamGetData();
+}
+
+function deleteteam() {
+    let teamdata = JSON.parse(localStorage.getItem("addteam")) || [];
+    let index = sessionStorage.getItem("index")
+    teamdata.splice(index, 1);
     localStorage.setItem("addteam", JSON.stringify(teamdata));
     window.location.href = "./teams.html"
+}
+
+function addmembers() {
+    let teamdata = JSON.parse(localStorage.getItem("addteam")) || [];
+    let addmembersinput = document.getElementById('addmembersinput').value;
+    let index = sessionStorage.getItem("index");
+    if (addmembersinput.length > 0) {
+        teamdata[index].email.push(addmembersinput);
+        localStorage.setItem("addteam", JSON.stringify(teamdata));
+        document.getElementById('addmembersinput').value = ""
+        settingteamGetData();
+    } else {
+        swal("Please Input First")
+    }
+
 }
